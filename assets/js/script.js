1f495b98d43a23f315d0d2cc87ef9ee1c271d0e1
Erik Lindén <erikbolinden@gmail.com>
2022-09-03 17:56:16 +0000

    add three digits to pick the lokc challenge and set min and max value to the fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,11 @@ function checkAnswer() {
 
     let firstDigit = document.getElementById("num1").value;
     let trueFirst = 6;
-    let result = firstDigit == trueFirst;
+    let secondDigit = document.getElementById("num2").value;
+    let trueSecond = 1;
+    let thirdDigit = document.getElementById("num3").value;
+    let trueThird = 0;
+    let result = firstDigit == trueFirst && secondDigit == trueSecond && thirdDigit == trueThird;
 
     if (result) {
         alert("Oh wow, we got ourselves a little thief! Good job, lets go...");
