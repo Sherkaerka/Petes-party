15e5a8981a70754e013cea73c99600c514fd86ed
Erik Lindén <erikbolinden@gmail.com>
2022-09-06 11:28:28 +0000

    add functionality to chapter 2 including challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,12 @@ function checkAnswer() {
 
     if (result) {
         alert("Oh wow, we got ourselves a little thief! Good job, lets go...");
+        incrementTime();
+        chapterTwo();
     } else {
         alert("I'm just guessing here and wasting my time! Think...think...");
         incrementTime();
+        chapterTwo();
     }
 
 }
@@ -29,39 +32,76 @@ function checkAnswer() {
  * Time Left Gauge
  */
   
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+function drawChart() {
 
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Time Left', actualTime]
-        ]);
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Time Left', actualTime]
+    ]);
 
-        var options = {
-          width: 400, height: 120,
-          redFrom: 9, redTo: 10,
-          yellowFrom:7, yellowTo: 9,
-          minorTicks: 5, max:10
-        };
+     var options = {
+        width: 400, height: 120,
+        redFrom: 9, redTo: 10,
+        yellowFrom:7, yellowTo: 9,
+        minorTicks: 5, max:10
+    };
 
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 
-        chart.draw(data, options);
-        }
+    chart.draw(data, options);
+    }
 
-        /**
-         * Add XX to the gauge everytime a try on a challenge has been made, no matter 
-         * wether it is right or wrong
-         */
-         function incrementTime() {
+/**
+* Add XX to the gauge everytime a try on a challenge has been made, no matter 
+* wether it is right or wrong
+*/
+function incrementTime() {
 
-          let oldTime = parseInt(document.getElementById("time").innerText);
-          document.getElementById("time").innerText = ++oldTime;
+    let oldTime = parseInt(document.getElementById("time").innerText);
+    document.getElementById("time").innerText = ++oldTime;
       
-      }
-        
-      function nextGame() {
-        location.href = "http://www.aftonbladet.se";
-      }
+}
+
+let actualTime = 0;
+
+/**
+ * Add chapter 2 and challenge
+ */
+function chapterTwo() {
+    document.getElementById("chapter-two").innerHTML += "<h2>Chapter 2: Get the flowers!</h2>";
+    
+
+    /**Input field */
+    let guessSum = document.createElement("input");
+    guessSum.setAttribute("id","sum1");
+    guessSum.setAttribute("type", "number");
+    document.getElementById("the-cashier").appendChild(guessSum);
+    
+    /**Submit answer button */
+    let sumButton = document.createElement("input");
+    sumButton.setAttribute("id","sum2");
+    sumButton.setAttribute("type", "button");
+    sumButton.value ="Try me";
+    sumButton.addEventListener("click", checkSecondAnswer);
+    document.getElementById("the-cashier").appendChild(sumButton);
+    
+
+    
+
+    function checkSecondAnswer() {
+        let cashierGuess = document.getElementById("sum1").value;
+        let trueCashier = 70;
+
+        let resultTwo = cashierGuess === trueCashier;
+
+        if (resultTwo) {
+            alert("Oh wow, we got ourselves a little thief! Good job, lets go...");
+        } else {
+            alert("I'm just guessing here and wasting my time! Think...think...");
+            incrementTime();
+        }
+    }
+}
